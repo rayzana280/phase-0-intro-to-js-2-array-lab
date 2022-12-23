1ea0cfd36129d814117dccdf71ca4accce95a56f
Ray Zana <rayzana280@gmail.com>
2022-12-22 23:03:12 -0500

    commit tree 93219dc92d2ac581186aea0f6756983405afd063

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats= ['Milo', 'Otis', 'Garfield']
+//const catsTwo= ['neyser', 'anthony', 'eros']
+
+function destructivelyAppendCat(name){
+   return  cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+    const cats2 = [...cats];
+function appendCat(name){
+    cats2.push(name)
+    return cats2;
+ }
+
+    const cats3 = [...cats];
+ function prependCat(name){
+    cats3.unshift(name);
+    return cats3;
+}
+
+const cats4 =[...cats]
+function removeLastCat(){
+    cats4.pop();
+    return cats4;
+}
+
+const cats5 = [...cats]
+function removeFirstCat(){
+    cats5.shift();
+    return cats5;
+}
